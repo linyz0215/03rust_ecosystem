9c052df33cc033264e0bda129ee57971ee577ca7
linyz0215 <1902829444@qq.com>
2026-01-10 12:47:44 +0800

    tracing_subscriber

--- examples/err.rs
+++ examples/err.rs
@@ -1,7 +1,29 @@
-use error_study::MyError;
-use std::{any, fs};
-use anyhow::Context;
+use std::fs;
 use std::num::ParseIntError;
+use thiserror::Error;
+#[derive(Error, Debug)]
+pub enum MyError {
+    #[error("I/O error:{0}")]
+    Io(#[from] std::io::Error),
+    #[error("Parse error:{0}")]
+    Parse(#[from] std::num::ParseIntError),
+    //#[error("Serialize json error: {0}")]
+    //Serialize(#[from] serde_json::Error),
+
+    #[error("Error: {0:?}")]
+    BigError(Box<BigError>),
+
+    #[error("Custom error: {0}")]
+    Custom(String),
+}
+#[allow(unused)]
+#[derive(Debug)]
+pub struct BigError {
+    a: String,
+    b: Vec<String>,
+    c: [u8;64],
+    d: u64,
+}
 fn main() -> Result<(), MyError> {
     println!("size of anyhow::Error is {}", size_of::<anyhow::Error>());
     println!("size of std::io::Error is {}", size_of::<std::io::Error>());
